398b040b6069eaf10edf09b96707f64a92dac11e
Peter Winton <pwinton@indigobio.com>
2017-05-06 00:06:20 -0400

    wip

--- lib/banzai.rb
+++ lib/banzai.rb
@@ -109,7 +109,34 @@ class Banzai
   (lambda (name)
     (ruby-call-proc "|x| Interop::ServiceShim.new(x)" name)))
 
+(define (make-proto class-name attrs)
+  (ruby-call-proc (++ "|attrs| " class-name ".new(attrs)") attrs))
 
+(define-syntax define-proto
+  (lambda (stx)
+    (let* ([name (caadr stx)]
+           [class-name (caaddr stx)]
+           [attr-names (cdadr stx)]
+           [maker-name (string->symbol (++ "make-" name))]
+           [attr-stx (cons 'make-map (flatmap (lambda (n) (list `(quote ,n) n)) attr-names))])
+      `(begin
+         (define (,maker-name ,@attr-names)
+           (make-proto ,class-name ,attr-stx))
+         (define ,name (list ,class-name ,maker-name (quote ,attr-names)))))))
+
+(define-syntax define-rpc
+  (lambda (stx)
+    (let* ([proc-name (caadr stx)]
+           [formals (cdadr stx)]
+           [rpc-info (caddr stx)]
+           [service-name (car rpc-info)]
+           [method (cadr rpc-info)]
+           [request-proto (caddr rpc-info)])
+      `(define (,proc-name ,@formals)
+         (puts (++ ',proc-name " " ,@formals))
+         (let* ([proto ,request-proto]
+                [maker (cadr proto)])
+           (,method (get-service ',service-name) (maker ,@formals)))))))
 EOD
     Rambda.eval(code, env)
   end
